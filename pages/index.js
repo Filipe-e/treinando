763baa7212f8a5779d2381af75f405c9fe3f995b
Filipe <fiilipe.com.i@gmail.com>
2025-05-12 13:16:39 +0000

    modifca a frase anterior adicionando um poema

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
     return <h1>
 olá para você que está lendo, saiba que para mim você é muito importante
-! ❣️
+! Saiba tambem que quero nossa amizade dure como um céu azul, mesmo que nunca negras tomem posse dele, lembrs-se
+depois das nuvens negras sempre há um lindo arco-íris 
     </h1>
 }
 
