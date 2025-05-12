0161697e2f32bbcdded083f3526ff4a9bddce32f
Filipe <fiilipe.com.i@gmail.com>
2025-05-12 13:57:56 +0000

    altera a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
     return <h1>
+☁️☁️   ☁️     ☁️             ☁️ ☁️ ☁️           ☁️ ☁️ ☀️
 olá para você que está lendo, saiba que para mim você é muito importante! Saiba tambem que quero nossa amizade dure como um céu azul, mesmo que nunca negras tomem posse dele, lembre-se
-depois das nuvens negras sempre há um lindo arco-íris 
+depois das nuvens negras sempre há um sol que brilha até que seja dia perfeito
     </h1>
 }
 
